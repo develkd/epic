ab9499409560c2438c24ff84086c9f4e416e19ce
develdk <develdk@gmail.com>
2017-07-25 22:42:38 +0200

    - Step 1: Actionbutton on list

--- app/src/main/java/de/master/kd/epic/view/position/PositionListActivity.java
+++ app/src/main/java/de/master/kd/epic/view/position/PositionListActivity.java
@@ -2,16 +2,19 @@ package de.master.kd.epic.view.position;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import de.master.kd.epic.R;
 import de.master.kd.epic.domain.interfaces.PositionService;
+import de.master.kd.epic.domain.position.Position;
 
 public class PositionListActivity extends AppCompatActivity {
 
     private PositionService positionService;
     private ListView customList;
-
+    private Position selectedPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,12 @@ public class PositionListActivity extends AppCompatActivity {
         customList = (ListView) findViewById(R.id.custom_list);
         PositionItemAdatper itemAdatper = new PositionItemAdatper(this,positionService.getPositions());
         customList.setAdapter(itemAdatper);
+        customList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                customList.getAdapter().getItem(position);
+            }
+        });
+
     }
 }
